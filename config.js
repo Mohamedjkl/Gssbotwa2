a21a4b6e2c69abcb1ad9636d42493ec995ee7ca1
Mohamedjkl <112547663+Mohamedjkl@users.noreply.github.com>
2024-07-31 18:55:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,11 +7,11 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = "mklnjkln";
+global.owner = ['48459234800'];
+global.premium = ['48459234800'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.author = "mklnjkln";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
@@ -29,16 +29,16 @@ global.autoBlock = true
  
 /*REPLY MESSAGE*/
 global.mess = {
-    banned: '`You are *Banned* fron using commands!`',
-    bangc: '`This Group is *Banned* from using Commands!`',
-    success: '`✓ Success`',
-    admin: '`This feature is only for group admins`',
-    botAdmin: '`I am not an admin!`',
-    owner: '`You are not my owner`',
-    group: '`You can use this command only in groups ❌`',
-    private: '`Feature is used only for private chats!`',
+    banned: '`انت محظور من استخدام هذا الكود`',
+    bangc: '`تم حظر الجروب`',
+    success: '`تم بنجاح`',
+    admin: '`هذا مسموح لي الادمن فقط`',
+    botAdmin: '`انا لست ادمن`',
+    owner: '`انت لست المالك`',
+    group: '`لا يمكن استخدمه الا في الجروبات`',
+    private: '`مسموح به في الشات الخاص فقط`',
     bot: '`Bot number user special features`',
-    wait: '*Processing Your request*',
+    wait: '*يعالج الطلب ...*',
     endLimit: '`Your daily limit has expired, the limit will be reset every 12 hours`',
 };
 
